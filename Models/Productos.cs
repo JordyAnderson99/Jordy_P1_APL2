dc7904d5554e41808d5073a968a65121a4ba595f
Jordy Anderson Celestino Mendoza <Jordy.Ander@hotmail.com>
2021-06-02 18:34:53 -0400

    Agregue los mensajes en la Models

--- Models/Productos.cs
+++ Models/Productos.cs
@@ -6,10 +6,21 @@ namespace Jordy_P1_APL2.Models
     public class Productos
     {
         [Key]
-        public int ProductoId { get; set; }
+        [Required(ErrorMessage = "No debe de estar Vacio el campo'ProductoId'")]
+        [Range(0, 100, ErrorMessage = "El Campo 'ProductoId' no puede ser 0 o (mayor a 1000),")]
+        public int PproductoId { get; set; }
+
+        [Required(ErrorMessage = "No debe de estar Vacio el campo'Descripcion'")]
+        [MinLength(4, ErrorMessage = "El Campo 'Descripcion' Debe tener por lo menos (4 caracteres),")]
+        [RegularExpression(@"\S(,*)\S", ErrorMessage = "Debe ser un texto,")]
         public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "No debe de estar Vacio el campo'Existencia'")]
         public decimal Existencia { get; set; }
+        [Required(ErrorMessage = "No debe de estar Vacio el campo'Costo'")]
         public decimal costo { get; set; }
+
+        [Required(ErrorMessage = "No debe de estar Vacio el campo'ValorInventario'")]
         public decimal ValorInventario { get; set; }
     }
 }
