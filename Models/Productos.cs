2ce8654728fe2dc232927bee47ef8fd12201c395
Jordy Anderson Celestino Mendoza <Jordy.Ander@hotmail.com>
2021-06-02 18:30:23 -0400

    Agregue la Models

---
+++ Models/Productos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Jordy_P1_APL2.Models
+{
+    public class Productos
+    {
+        [Key]
+        public int ProductoId { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Existencia { get; set; }
+        public decimal costo { get; set; }
+        public decimal ValorInventario { get; set; }
+    }
+}
