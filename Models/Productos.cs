1292727db731e498add09d63c43030367f7a9c3c
Jordy Anderson Celestino Mendoza <Jordy.Ander@hotmail.com>
2021-06-02 19:15:49 -0400

    Agregue la clase ProductoBLL y Modifique en la clase Productos el ProductoId y el costo

--- Models/Productos.cs
+++ Models/Productos.cs
@@ -8,7 +8,7 @@ namespace Jordy_P1_APL2.Models
         [Key]
         [Required(ErrorMessage = "No debe de estar Vacio el campo'ProductoId'")]
         [Range(0, 100, ErrorMessage = "El Campo 'ProductoId' no puede ser 0 o (mayor a 1000),")]
-        public int PproductoId { get; set; }
+        public int ProductoId { get; set; }
 
         [Required(ErrorMessage = "No debe de estar Vacio el campo'Descripcion'")]
         [MinLength(4, ErrorMessage = "El Campo 'Descripcion' Debe tener por lo menos (4 caracteres),")]
@@ -18,7 +18,7 @@ namespace Jordy_P1_APL2.Models
         [Required(ErrorMessage = "No debe de estar Vacio el campo'Existencia'")]
         public decimal Existencia { get; set; }
         [Required(ErrorMessage = "No debe de estar Vacio el campo'Costo'")]
-        public decimal costo { get; set; }
+        public decimal Costo { get; set; }
 
         [Required(ErrorMessage = "No debe de estar Vacio el campo'ValorInventario'")]
         public decimal ValorInventario { get; set; }
